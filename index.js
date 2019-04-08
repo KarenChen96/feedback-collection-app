07e36160cb8fd7476faff2a651e0e364e93d554f
hp <hp@chenchuns-MacBook-Pro.local>
2019-04-07 18:43:18 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+// Dynamic figure  out what port we should listen to.
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
